6a4bc336e52fdad7d871a1e5289fcb49d293ac64
chris-perficient <christopher.mcardle@perficient.com>
2022-05-18 14:36:31 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,161 @@
+/*
+Welcome! 
+
+In your web browser, go to the article you want to copy.                                             !~~~~~~~~~~!
+Copy the article's ID number from the URL (Example: https://perficient.zendesk.com/hc/en-us/articles/360046283354-Welcome-to-the-CPD-Team)
+
+Paste that ID number between the quotation marks on the line below
+*/
+const articleToBeMoved = "360047225853";
+
+/*
+Use the list below to look up the ID number of the section that the article is located in.
+Copy the number the number at the end of that line, and paste it between the quotation marks below.
+*/
+const fromSection = "360008436633";
+/*
+
+/* 
+Please choose which specific Wiki you want the article to transfer into, and paste between quotes on line below.
+You can choose from the options- "Wikis", "Technical", "Soft-Skills", or "General".
+*/
+const whichWiki = "General";
+
+/*
+To create the Page in a specific folder, carefully type the folder name in the line below.
+Please ensure that it matches the folder name exactly. (same capital)
+*/
+const folder = "";
+
+/*
+Click "File", then "Save", to save the current selections.
+From the top ribbon, select "Terminal", and then "New Terminal"
+In the terminal window at the bottom, type 'npm start' and hit Enter.
+*/
+
+
+
+
+
+/*
+
+******************** Below is a list of Sections and their cooresponding ID #s ********************
+
+Twilio Flex - Knowledge Base Help - Getting Started = 360007827413
+Twilio Flex - Knowledge Base Help - FAQ = 360007771634
+Twilio Flex - Onboarding - Getting Started = 360008115314
+Twilio Flex - Onboarding - Twilio Platform and Twilio Flex - 360008206973
+Twilio Flex - Onboarding - Cloud Platform Usage = 360008436753
+Twilio Flex - Onboarding - Contact Center = 360007828213
+Twilio Flex - General Team Information/Resources - Administrative = 360008582593
+Twilio Flex - General Team Information/Resources - FAQ = 360002747613
+Twilio Flex - General Team Information/Resources - Practices, Processes, Procedures = 360002747593
+Twilio Flex - General Team Information/Resources - Interviewing Materials/Process = 4408234374163
+Twilio Flex - Developer - Logging = 360008209893
+Twilio Flex - Developer - Code Sample = 360008040594
+Twilio Flex - Developer - Tutorials and Research = 360008029154
+Twilio Flex - Developer - Internal IP = 360008028994
+Twilio Flex - Developer - Flex Documentation = 360007924234
+Twilio Flex - Twilio Support - Twilio SIGNAL Conference Sessions = 360008661193
+Twilio Flex - Twilio Support - Procedures = 360008482613
+Twilio Flex - Cloud Platform Usage - Platform = 360011720573
+
+Amazon Connect - MSO Internal Documentation - PACE = 1500001355681
+Amazon Connect - MSO Internal Documentation - Process & Procedures = 360010640954
+Amazon Connect - MSO Internal Documentation - Troubleshooting = 360011767213
+Amazon Connect - Managed Service Customer Documentation - Amazon Customers = 4405824645651
+Amazon Connect - Managed Service Customer Documentation - PACE Customers = 4405824643603
+Amazon Connect - PACE Customer Documentation - Agent Console = 4405816123411
+Amazon Connect - PACE Customer Documentation - Prompts Management = 4405824105363
+Amazon Connect - PACE Customer Documentation - Calendar Management = 4405824547091
+Amazon Connect - PACE Customer Documentation - Handling = 4405816428691
+Amazon Connect - PACE Customer Documentation - Widget Management = 4405816422547
+Amazon Connect - PACE Customer Documentation - Lex Bot Management = 4405816425875
+Amazon Connect - PACE Customer Documentation - Settings Management = 4405824583443
+Amazon Connect - PACE Customer Documentation - Reports = 1500001755702
+Amazon Connect - PACE Customer Documentation - Other Help Articles = 4551331293843
+Amazon Connect - AWS - Contact Lens = 4410663138195
+Amazon Connect - AWS - Lex V1 = 4408191519123
+Amazon Connect - AWS - Lex V2 = 4564958881171
+Amazon Connect - AWS - Voice ID = 4416889490195
+Amazon Connect - AWS - Reporting & Metrics Labs = 4405384322835
+Amazon Connect - AWS - Contact Flows = 4406974986643
+Amazon Connect - AWS - Training & Tutorials = 360008568254
+Amazon Connect - Onboarding - Getting Started = 360008182373
+Amazon Connect - Onboarding - Amazon Connect = 360007828273
+Amazon Connect - Onboarding - Cloud Platform Usage = 360009366793
+Amazon Connect - Onboarding - Contact Center = 360007828253
+Amazon Connect - Developer Resources - General = 360008436633
+Amazon Connect - Developer Resources - AWS = 360008339714
+Amazon Connect - Developer Resources - React = 360008436533
+Amazon Connect - Developer Resources - Testing = 360008436553
+Amazon Connect - Developer Resources - Tutorials and Research = 360008339654
+Amazon Connect - General Team Information/Resources - Administrative = 360008362394
+Amazon Connect - General Team Information/Resources - FAQ = 360008339694
+Amazon Connect - General Team Information/Resources - Practices, Processes, & Procedures = 360008339674
+Amazon Connect - Knowledge Base Help - Getting Started = 360007827453
+Amazon Connect - Knowledge Base Help - FAQ = 360007827433
+
+MSO - Internal Documentation - DevOps = 4416429291923
+MSO - Internal Documentation - PACE KB articles = 4404373597715
+MSO - Internal Documentation - General = 1500000751421
+MSO - Internal Documentation - Training & Development = 360011745553
+MSO - Internal Documentation - Troubleshooting = 360010600454
+MSO - Internal Documentation - Error Code Investigation = 360011720593
+MSO - Internal Documentation - Twilio Support = 360010600434
+MSO - Internal Documentation - KB Guidelines & Best Practices = 360010585854
+MSO - Internal Documentation - Process & Procedures = 360008291414
+MSO - Twilio Documentation - General = 1500000774282
+MSO - Twilio Documentation - Twilio Flex Release Notes = 360004915093
+MSO - Twilio Documentation - Troubleshooting = 1500000746781
+MSO - Twilio Documentation - Agent & Supervisor Training Documentation- Twilio Native Functionality = 360011704913
+MSO - Customer Documentation - General = 360011720413
+MSO - Customer Documentation - Innocean- Hyundai = 1500000746761
+MSO - Customer Documentation - Neiman Marcus = 1500000633701
+MSO - Customer Documentation - 7-Eleven = 360010523174
+MSO - Customer Documentation - Mathematica = 360011045213
+MSO - Customer Documentation - Washington Crisis Connections = 360008935693
+MSO - Customer Documentation - Perficient Internal = 360008315934
+MSO - Customer Documentation - Aritzia = 360007645993
+MSO - Customer Documentation - CompuCom = 360007615973
+MSO - Customer Documentation - Red Rock Technologies (ALC Schools) = 360006113333
+MSO - Customer Documentation - American Logistics Corporation (ALC Med/Gov) = 360005657753
+MSO - Customer Documentation - Nike = 360005657173
+
+Clarity Connect - Clarity Connect - Connect Help = 360008920733
+Clarity Connect - Clarity Connect - Agent Console = 360005262593
+Clarity Connect - Clarity Connect - Wesbite = 360005224994
+Clarity Connect - Clarity Connect - Chat and Calls = 360005224974
+Clarity Connect - Clarity Connect - General Guidelines = 360005262573
+Clarity Connect - Clarity Connect - Dashboard = 360005262553
+Clarity Connect - Clarity Connect - Reports = 360005224954
+Clarity Connect - Clarity Connect - Maintenance Configuration = 360005262533
+Clarity Connect - Clarity Connect - IVRs / Call Flows = 360005224934
+Clarity Connect - Clarity Connect - Agent Call Rerouter = 360005262493
+Clarity Connect - Clarity Connect - Logs = 360005262473
+Clarity Connect - Clarity Connect - Pre-Implementation = 360005262453
+Clarity Connect - Clarity Connect - Web Gateway = 360005224914
+Clarity Connect - Clarity Connect - Installations and Upgrades = 360005262433
+Clarity Connect - Clarity Connect - Connect Application Server Maintenance = 360005262413
+Clarity Connect - Clarity Connect - Clarity Connect 5.0 = 360005262393
+Clarity Connect - Clarity Connect - Partner Guidelines = 360005224834
+Clarity Connect - Clarity Connect - Clarity Connect 4.1 = 360005224814
+Clarity Connect - Internal Team Guidelines - Internal instances and best practices = 360005225034
+Clarity Connect - Internal Team Guidelines - Ticket handling = 360005225014
+Clarity Connect - Internal Team Guidelines - Internal = 360005262513
+Clarity Connect - Internal Team Guidelines - CMP/Installation Manager - Internal = 360005224854
+
+Converge - Customer Documentation - Intrado/West = 1500002395662
+Converge - Clarity Converge - Troubleshooting = 360005223334
+Converge - Clarity Converge - Admin Website = 360005261253
+Converge - Clarity Converge - General = 360005261233
+Converge - Clarity Converge - Lync Environment = 360005223314
+Converge - Clarity Converge - Topology Sync = 360005223294
+Converge - Clarity Converge - Integration Service = 360005223274
+Converge - Clarity Converge - Registrar Service = 360005223254
+
+*/
+exports.articleToBeMoved = articleToBeMoved;
+exports.fromSection = fromSection;
+exports.whichWiki = whichWiki;
+exports.folder = folder;
---
+++ transferPage.js
@@ -0,0 +1,186 @@
+/* Copyright (c) 2021 Perficient, Inc. - All Rights Reserved.
+ *
+ *  Unauthorized copying of this file, via any medium is strictly prohibited.
+ *  Proprietary and confidential.
+ */
+
+"use strict";
+require("dotenv").config();
+const axios = require("axios");
+const token = process.env.ZENDESK_TOKEN;
+const encoded = Buffer.from(
+  "christopher.mcardle@perficient.com/token:" + token
+).toString("base64");
+
+const azAuth = Buffer.from(process.env.AZURE_BEARER_TOKEN).toString("base64");
+const { articleToBeMoved, fromSection, whichWiki, folder } = require("./main");
+
+const twilioFlexIds = [
+  "360007827413",
+  "360007771634",
+  "360008115314",
+  "360008206973",
+  "360008436753",
+  "360007828213",
+  "360008582593",
+  "360002747613",
+  "360002747593",
+  "4408234374163",
+  "360008209893",
+  "360008040594",
+  "360008029154",
+  "360008028994",
+  "360007924234",
+  "360008661193",
+  "360008482613",
+  "360011720573",
+];
+const amazonConnectIds = [
+  "360007827433",
+  "360007827453",
+  "360008339674",
+  "360008339694",
+  "360008362394",
+  "360008339654",
+  "360008436553",
+  "360008436533",
+  "360008339714",
+  "360008436633",
+  "360008182373",
+  "360007828273",
+  "360009366793",
+  "360007828253",
+  "4406974986643",
+  "4406974986643",
+  "4405384322835",
+  "4416889490195",
+  "1500001355681",
+  "360010640954",
+  "360011767213",
+  "4405824645651",
+  "4405824643603",
+  "4405816123411",
+  "4405824105363",
+  "4405824547091",
+  "4405816428691",
+  "4405816422547",
+  "4405816425875",
+  "4405824583443",
+  "1500001755702",
+  "4551331293843",
+  "4410663138195",
+  "4408191519123",
+  "4564958881171",
+];
+const msoIds = [
+  "360005657173",
+  "360005657753",
+  "360006113333",
+  "360007615973",
+  "360007645993",
+  "360008315934",
+  "360008935693",
+  "360011045213",
+  "360010523174",
+  "1500000633701",
+  "1500000746761",
+  "360011720593",
+  "360010600454",
+  "360011745553",
+  "1500000751421",
+  "4404373597715",
+  "4416429291923",
+  "360010600434",
+  "360010585854",
+  "360008291414",
+  "1500000774282",
+  "360004915093",
+  "1500000746781",
+  "360011704913",
+  "360011720413",
+];
+const clarityConnectIds = [
+  "360008920733",
+  "360005262593",
+  "360005224994",
+  "360005224974",
+  "360005262573",
+  "360005262553",
+  "360005224954",
+  "360005262533",
+  "360005224934",
+  "360005262493",
+  "360005262473",
+  "360005262453",
+  "360005224914",
+  "360005262433",
+  "360005262413",
+  "360005262393",
+  "360005224834",
+  "360005224814",
+  "360005225034",
+  "360005225014",
+  "360005262513",
+  "360005224854",
+];
+const convergeIds = [
+  "1500002395662",
+  "360005223334",
+  "360005261253",
+  "360005261233",
+  "360005223314",
+  "360005223294",
+  "360005223274",
+  "360005223254",
+];
+
+// Fetches the article to be moved
+async function getArticle() {
+  let domainName;
+  if (twilioFlexIds.includes(fromSection)) {
+    domainName = "perficient";
+  } else if (amazonConnectIds.includes(fromSection)) {
+    domainName = "prftamazonconnect";
+  } else if (clarityConnectIds.includes(fromSection)) {
+    domainName = "prft-cconnect";
+  } else if (convergeIds.includes(fromSection)) {
+    domainName = "prft-converge";
+  } else if (msoIds.includes(fromSection)) {
+    domainName = "prft-mso";
+  } else {
+    console.log("COULDNT FIND A MATCH. PLEASE ENSURE YOUR IDS ARE ACCURATE");
+    return;
+  }
+  axios
+    .get(
+      `https://${domainName}.zendesk.com/api/v2/help_center/en-us/articles/${articleToBeMoved}`,
+      { headers: { Authorization: `Basic ${encoded}` } }
+    )
+    .then((res) => {
+      moveArticle(res.data);
+      return;
+    })
+    .catch((err) => console.log("ERROR", err.message));
+}
+// Creates the page in Azure Wiki
+async function moveArticle(args) {
+  const { title, body } = args.article;
+  const headers = {
+    Accept: "application/json",
+    Authorization: `Basic ${azAuth}`,
+  };
+  const url = folder
+    ? `https://dev.azure.com/PRFTMSFTCDS/Wikis/_apis/wiki/wikis/${whichWiki}.wiki/pages?path=${folder}/${title}&versionDescriptor.version=master&versionDescriptor.versionType=branch&api-version=6.0`
+    : `https://dev.azure.com/PRFTMSFTCDS/Wikis/_apis/wiki/wikis/${whichWiki}.wiki/pages?path=${title}&versionDescriptor.version=master&versionDescriptor.versionType=branch&api-version=6.0`;
+
+  try {
+    axios
+      .put(url, { content: body }, { headers })
+      .then((res) => console.log("Status", res.status));
+  } catch (err) {
+    console.log("ERROR", err.message);
+  }
+}
+
+getArticle();
+
